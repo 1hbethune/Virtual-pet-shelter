d1181bbd752743f51c0a2c42abebb5f199f60c1c
1hbethune <1hbethune@att.net>
2018-09-18 17:18:39 -0400

    Initial Push

---
+++ README.md
@@ -0,0 +1,20 @@
+# Big Header
+
+## SLightly Smaller Header
+
+### Even Smaller header
+
+*italics*
+
+**bold**
+
+* Item list
+	* insert item listf
+
+	asldhfasdlfnalng
+	;adsfnalsdnf
+
+
+	adslkknfal
+
+	aselfan
---
+++ src/main/java/VirtualPet.java
@@ -0,0 +1,93 @@
+
+public class VirtualPet {
+
+	private int currentHunger;
+	private int currentThirst;
+	private int currentWaste;
+	private int currentBoredom;
+	private int currentSickness;
+	private int currentTiredness;
+
+	public VirtualPet(int currentHunger, int currentThirst, int currentWaste, int currentBoredom, int currentSickness,
+			int currentTiredness) {
+		this.currentHunger = currentHunger;
+		this.currentThirst = currentThirst;
+		this.currentWaste = currentWaste;
+		this.currentBoredom = currentBoredom;
+		this.currentSickness = currentSickness;
+		this.currentTiredness = currentTiredness;
+
+	}
+
+	public int getCurrentHunger() {
+		return currentHunger;
+	}
+
+	public void feed(int foodAmount) {
+		currentHunger -= foodAmount;
+		currentThirst += foodAmount;
+		currentWaste += foodAmount;
+
+	}
+
+	public int getCurrentThirst() {
+		return currentThirst;
+	}
+
+	public void giveWater(int waterAmount) {
+		currentThirst -= waterAmount;
+
+	}
+
+	public int getCurrentWaste() {
+		return currentWaste;
+	}
+
+	public void takenOut(int usedPotty) {
+		currentWaste -= usedPotty;
+
+	}
+
+	public int getCurrentBoredom() {
+		return currentBoredom;
+	}
+
+	public void playWith(int play) {
+		currentBoredom -= play;
+		currentTiredness += play;
+
+	}
+
+	public int getCurrentSickness() {
+		return currentSickness;
+	}
+
+	public void takeToVet(int vet) {
+		currentSickness -= vet;
+
+	}
+
+	public int getCurrentTiredness() {
+		return currentTiredness;
+	}
+
+	public void putToSleep(int sleep) {
+		currentTiredness -= sleep;
+
+	}
+
+	public void tick() {
+		currentBoredom += 5;
+		currentHunger += 5;
+		currentTiredness += 5;
+
+	}
+
+	@Override
+	public String toString() {
+		return "VirtualPet [currentHunger=" + currentHunger + ", currentThirst=" + currentThirst + ", currentWaste="
+				+ currentWaste + ", currentBoredom=" + currentBoredom + ", currentSickness=" + currentSickness
+				+ ", currentTiredness=" + currentTiredness + "]";
+	}
+
+}
